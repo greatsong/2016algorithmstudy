02d2ea5353036eccb269055715591508bac4b02b
greatsong <greatsong21@gmail.com>
2016-02-09 17:53:45 +0900

    quick sort 일부 주석 처리

--- 160208quicksort.cpp
+++ 160208quicksort.cpp
@@ -4,9 +4,34 @@
 #include <algorithm> //xcode에서 실행할 경우 확장자를 cpp로 할 것
 
 int n, S[1000000];
+using namespace std;
 
-//S배열의 내용 출력, 0~n-1까지
-//정렬 전과 정렬 후에 2번 사용되기 때문에 재사용성을 높이기 위해 함수로 구현
+void swap(int a, int b){
+    int t = S[a];
+    S[a] = S[b];
+    S[b] = t;
+}
+
+//s : start, e : end
+void quick_sort(int s, int e){
+    //start와 end가 뒤바뀌거나 같아지면 함수 종료
+    if(s<e){
+        //p : pivot, l : left, r : right
+        int p = s, l = s+1, r = e;
+        while(l<=r){
+            while(l<=e && S[l]<=S[p])
+                l++;
+            while(r>=s+1 && S[r]>=S[p])
+                r--;
+            if(l<r)
+                swap(l,r);
+        }
+        swap(p, r);
+        quick_sort(s, r-1);
+        quick_sort(r+1, e);
+    }
+}
+
 void print_array(){
     for(int i=0;i<n;i++)
         printf("%3d\t",S[i]);
@@ -20,9 +45,9 @@ int main(){
         S[i]=rand();
     //print_array();
     int start = clock();
-    //standard 라이브러리 사용, 퀵 정렬
-    std::sort(S,S+n);
+
+    quick_sort(0, n-1);
     printf("result=%.3lf(sec)\n",(double)(clock()-start)/CLOCKS_PER_SEC);
-    //print_array();
+    print_array();
     return 0;
 }
